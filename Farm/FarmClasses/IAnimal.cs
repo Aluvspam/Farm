04783430762a671e4e21f787dbe637ba013060f8
andrei patrascu <andreipatrascu@outlook.com>
2019-04-26 15:22:42 +0300

    created FarmClasses project containing IAnimal, and WurmAnimal

---
+++ Farm/FarmClasses/IAnimal.cs
@@ -0,0 +1,14 @@
+﻿using FarmClasses.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmClasses
+{
+    interface IAnimal
+    {
+        Species Species { get; }
+    }
+}
---
+++ Farm/FarmClasses/WurmAnimal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FarmClasses.Enums;
+
+namespace FarmClasses
+{
+    class WurmAnimal : IAnimal
+    {
+        Species species;
+        public Species Species
+        {
+            get
+            {
+                return species;
+            }
+        }
+        public WurmAnimal(Species species)
+        {
+            this.species = species;
+        }
+    }
+}
